2a9abeadaea984a9aeb1ed00bd9aeed01ab7c780
Ali <aligadallah14@gmail.com>
2024-09-08 18:20:07 -0400

    Worked on Signup Validation, Still not Finished

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const { body } = require('express-validator/check');
 
+const User = require('../models/user')
+
+const authController = require('../controllers/auth');
+
 const router = express.Router();
 
-router.get('signup');
+router.get('signup', authController.getSignUp);
 
-router.post('signup');
+router.post('signup', [
+    body('email').isEmail().withMessage('Please Enter a Valid Email')
+    .custom((value, req) => {
+        return User.findByEmail(email).then(user => {
+            if (user) {
+                Promise.reject('Email Already Exists');
+            }
+        })
+    })
+], authController.postSignUp);
 
-router.get('login');
+router.get('login', authController.getLogin);
 
-router.post('login');
+router.post('login', [], authController.postLogin);
 
 module.exports = router;
